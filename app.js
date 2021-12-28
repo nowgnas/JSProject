f16c7c380b03c52e30a8bc3f3cf924903f774ed2
s-wlii <marshmello.sw@gmail.com>
2021-12-28 22:35:52 +0900

    amend file
    lec 3.4 fin

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const age = 96;
+const title = document.querySelector("div.hello:first-child h1");
 
-function calculateKrage(ageOfForeigner) {
-  return ageOfForeigner + 2;
+function handleTitleClick() {
+  title.style.color = "blue";
 }
 
-const krAge = calculateKrage(age);
+function handleMousEnter() {
+  title.innerText = "mouse is here";
+}
 
-console.log(krAge);
+function handleMouseLeave() {
+  title.innerText = "mouse is gone";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMousEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
